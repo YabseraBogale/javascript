a58bf59cc50949655512ec52fa44e45b694d904f
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-09 18:41:29 +0300

    Add files via upload

---
+++ seme.js
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4,5,6,7]
+console.log("for loop")
+for(let i=0;i<arr.length;i++){
+	console.log(arr[i])
+	}
+let i=0
+console.log("while loop")
+while(i<arr.length){
+	console.log(arr[i])
+	i++
+	}
