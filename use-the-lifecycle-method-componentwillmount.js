fd7ce247926fafecccb88d8c0c2a2b0ffc5d5787
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-01-20 16:12:27 +0300

    Add files via upload

---
+++ use-the-lifecycle-method-componentwillmount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+        <div>console.log("Hello")</div>
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
