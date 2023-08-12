4216027d59323063de42e820c65ccffd526ba264
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-08-12 19:27:58 +0300

    Add files via upload

---
+++ whatAboutArrays.js
@@ -0,0 +1,6 @@
+//test Array multipcation
+
+console.log([1,2,3]*1) //return NaN
+console.log([1,2,3]+1) //return is string type where the last element will be add 1,2,31
+console.log([1,2,3]-1)//return NaN
+console.log([1,2,3]/1)//return NaN
